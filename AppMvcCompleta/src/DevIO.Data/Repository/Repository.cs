12ee86d7b17adc342f7c6bed997329fc01616d0a
David <davidroliveira@gmail.com>
2021-09-07 18:15:26 -0300

    Aula: Acessando o banco via repositórios

---
+++ AppMvcCompleta/src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DevIO.Business.Interfaces;
+using AppMvcBasica.Models;
+using DevIO.Data.Context;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly MeuDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(MeuDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<TEntity> ObterPorId(Guid Id)
+        {
+            return await DbSet.FindAsync(Id);
+        }
+
+        public virtual async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+            //Db.Set<TEntity>().Add(entity); //Alternativa mais verbosa
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            //DbSet.Remove(await DbSet.FindAsync(id)); //Cria objeto e depois remove mas é processamento desnecessario
+            var entity = new TEntity { Id = id };
+            DbSet.Remove(entity);
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+
+    }
+}
