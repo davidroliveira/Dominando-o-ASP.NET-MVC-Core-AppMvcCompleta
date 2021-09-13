a8b4883054b0ab166bbe9e1c55c662bc0fba876e
David <davidroliveira@gmail.com>
2021-09-13 20:59:48 -0300

    Aula: Validando as entidades de negócio

---
+++ AppMvcCompleta/src/DevIO.Business/Services/ProdutoService.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
